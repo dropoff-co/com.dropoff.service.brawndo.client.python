5d969f675e2a89e60f197d30d844f0f111811f6a
Jason Kastner <jkastner@dropoff.com>
2017-07-12 13:12:40 -0500

    v1.0.3 * Some python idiom fixes

--- DropoffApi/ApiV1.py
+++ DropoffApi/ApiV1.py
@@ -4,8 +4,8 @@ from Order import Order
 
 class ApiV1:
     def __init__(self, api_url, base_path, host, private_key, public_key):
-        self.client = Client(api_url, base_path, host, private_key, public_key)
-        self.order = Order(self.client)
+        self._client = Client(api_url, base_path, host, private_key, public_key)
+        self.order = Order(self._client)
 
     def info(self):
-        return self.client.do_get('/info', 'info')
+        return self._client.do_get('/info', 'info')
